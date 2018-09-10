712b9d8ea6d26232392f2633ef4b4ea1d14fc042
Jian Han <jianhan@mail.com>
2018-09-10 19:59:36 +1000

    [refactor] Added zomato route struct

--- routes/routes.go
+++ routes/routes.go
@@ -3,12 +3,21 @@ package routes
 import (
 	"github.com/iris-contrib/middleware/cors"
 	"github.com/jianhan/goiris/bootstrap"
+	"github.com/jianhan/goiris/zomato"
 	"github.com/kataras/iris/cache"
+	"os"
 	"time"
 )
 
 // Configure registers the necessary routes to the app.
 func Configure(b *bootstrap.Bootstrapper) {
+	commonAPI, err := zomato.NewCommonAPI(os.Getenv("ZOMATO_API_KEY"), os.Getenv("ZOMATO_API_URL"))
+	if err != nil {
+		panic(err)
+	}
+
+	zomatonRoutes := newZomatoRoutes(commonAPI)
+
 	// setup API routes
 	crs := cors.New(cors.Options{
 		AllowedOrigins:   []string{"*"}, // allows everything, use that to change the hosts.
@@ -28,8 +37,8 @@ func Configure(b *bootstrap.Bootstrapper) {
 		// zomato
 		zomatoRoutes := apiV1Routes.Party("/zomato")
 		{
-			zomatoRoutes.Get("/categories", cacheHandler, GetZomatoCategoriesHandler)
-			zomatoRoutes.Get("/cities", cacheHandler, GetZomatoCategoriesHandler)
+			zomatoRoutes.Get("/categories", cacheHandler, zomatonRoutes.GetZomatoCategoriesHandler)
+			zomatoRoutes.Get("/cities", cacheHandler, zomatonRoutes.GetZomatoCitiesHandler)
 		}
 	}
 }
--- routes/zomato.go
+++ routes/zomato.go
@@ -6,23 +6,18 @@ import (
 	"github.com/jianhan/goiris/zomato"
 	"github.com/kataras/iris"
 	"net/http"
-	"os"
 )
 
-func GetZomatoCategoriesHandler(ctx iris.Context) {
-	commonAPI, err := zomato.NewCommonAPI(os.Getenv("ZOMATO_API_KEY"), os.Getenv("ZOMATO_API_URL"))
-	if err != nil {
-		ctx.StatusCode(iris.StatusInternalServerError)
-		ctx.JSON(
-			ghttp.HttpError{
-				Message: fmt.Sprintf("system error, invalid configuration , %s", err.Error()),
-				Status:  http.StatusInternalServerError,
-			},
-		)
-		return
-	}
+type zomatoRoutes struct {
+	commonAPI zomato.CommonAPI
+}
 
-	categories, err := commonAPI.Categories()
+func newZomatoRoutes(commonAPI zomato.CommonAPI) *zomatoRoutes {
+	return &zomatoRoutes{commonAPI: commonAPI}
+}
+
+func (z *zomatoRoutes) GetZomatoCategoriesHandler(ctx iris.Context) {
+	categories, err := z.commonAPI.Categories()
 	if err != nil {
 		ctx.StatusCode(iris.StatusInternalServerError)
 		ctx.JSON(
@@ -36,4 +31,21 @@ func GetZomatoCategoriesHandler(ctx iris.Context) {
 
 	ctx.StatusCode(iris.StatusOK)
 	ctx.JSON(categories)
+}
+
+func (z *zomatoRoutes) GetZomatoCitiesHandler(ctx iris.Context) {
+	//categories, err := z.commonAPI.Categories()
+	//if err != nil {
+	//	ctx.StatusCode(iris.StatusInternalServerError)
+	//	ctx.JSON(
+	//		ghttp.HttpError{
+	//			Message: fmt.Sprintf("system error, unable to retrieve categories, %s", err.Error()),
+	//			Status:  http.StatusInternalServerError,
+	//		},
+	//	)
+	//	return
+	//}
+	//
+	//ctx.StatusCode(iris.StatusOK)
+	//ctx.JSON(categories)
 }
